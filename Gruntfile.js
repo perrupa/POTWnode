de1ee9ec55ac76342d0b93d35a8e49a8e4b237ca
Christopher Marlow <christopher.marlow@gmail.com>
2013-12-14 16:23:02 -0500

    Initial commit with npm and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+	// Grunt's Task Configs
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		jshint: {
+			files: [
+				'Gruntfile.js', // This file
+				'**/*.js' // All other JS
+			],
+			options: {
+				// options here to override JSHint defaults
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true
+				}
+			}
+		}
+
+	});
+
+	// Load Tasks
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+
+	// Register Compound Tasks
+	grunt.registerTask('test', ['jshint']);
+
+};
