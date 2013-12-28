69c126bfaa2c1e468f6187feb808a65254ad54bf
Christopher Marlow <christopher.marlow@gmail.com>
2013-12-28 15:17:14 -0500

    Added a debug task to the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,15 +18,60 @@ module.exports = function(grunt) {
 					document: true
 				}
 			}
+		},
+
+		nodemon: {
+			dev: {
+				options: {
+					file: 'app.js',
+					nodeArgs: ['--debug'],
+					env: {
+						PORT: '3000'
+					}
+				}
+			}
 		}
 
 	});
 
 	// Load Tasks
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-nodemon');
 
 
 	// Register Compound Tasks
 	grunt.registerTask('test', ['jshint']);
+
+	grunt.registerTask('debug', 'Start Node.js and node-inspector.', function() {
+		var done = this.async(),
+			nodeConfig = {
+				cmd: 'node',
+				args: ['--debug', 'app.js'],
+				opts: {
+					//cwd: current workin directory
+				}
+			},
+			nodeInspectorConfig = {
+				cmd: 'node-inspector',
+				args: ['&'],
+				opts: {
+					//cwd: current workin directory
+				}
+			},
+			doneFunction = function (msg) {
+				grunt.log.writeln(msg);
+
+				return function (error, result, code) {
+					if (error) {
+						grunt.log.write (result);
+						grunt.fail.fatal(error);
+					}
+					done();
+				}
+			};
+
+		grunt.util.spawn(nodeConfig, doneFunction("Node.js Server started"));
+		grunt.util.spawn(nodeInspectorConfig, doneFunction("node-inspector started"));
+	});
 
 };
